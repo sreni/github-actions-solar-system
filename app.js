fc031935fefae8a0c21ab2653a0c2d9e4287f9e1
sreni <33428674+sreni@users.noreply.github.com>
2025-07-23 18:21:04 +0530

    added quotess

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.mongodb+srv://supercluster.d83jj.mongodb.net/superData, {
+mongoose.connect(process.env.`mongodb+srv://supercluster.d83jj.mongodb.net/superData`, {
     user: process.env.superuser,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
